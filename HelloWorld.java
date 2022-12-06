e026181cd936d92fb2d9ac58a883675c84262d3e
kommirisukanya <114582527+kommirisukanya@users.noreply.github.com>
2022-12-06 08:18:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Welcome to jenkins DashBoard");
+    System.out.println("Hello, Welcome to World");
   }
 }
